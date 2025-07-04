730c7aceaadf858d25e7d947f3066a8f24dd3537
Martin Voogt <M.Voogt@kennisnet.nl>
2025-07-04 13:48:42 +0200

    Observables

--- apm-begin/src/app/products/product-detail/product-detail.component.ts
+++ apm-begin/src/app/products/product-detail/product-detail.component.ts
@@ -1,55 +1,33 @@
-import {
-  Component,
-  inject,
-  Input,
-  OnChanges,
-  OnDestroy,
-  SimpleChanges,
-} from '@angular/core';
-
-import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { NgIf, NgFor, CurrencyPipe, AsyncPipe } from '@angular/common';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
-import { catchError, EMPTY, Subscription } from 'rxjs';
+import { catchError, EMPTY } from 'rxjs';
 
 @Component({
   selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, CurrencyPipe],
+  imports: [NgIf, NgFor, CurrencyPipe, AsyncPipe],
 })
-export class ProductDetailComponent implements OnChanges, OnDestroy {
+export class ProductDetailComponent {
   private productService = inject(ProductService);
 
-  @Input() productId: number = 0;
   errorMessage = '';
 
   // Product to display
-  product: Product | null = null;
-  productSub!: Subscription;
+  product$ = this.productService.product$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
 
   // Set the page title
-  pageTitle = this.product
+  pageTitle = 'ProductDetail';
+  /*   pageTitle = this.product
     ? `Product Detail for: ${this.product.productName}`
-    : 'Product Detail';
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const productId = changes['productId'].currentValue;
-    if (productId)
-      this.productSub = this.productService
-        .getProduct(productId)
-        .pipe(
-          catchError(err => {
-            this.errorMessage = err;
-            return EMPTY;
-          })
-        )
-        .subscribe(product => (this.product = product));
-  }
-
-  ngOnDestroy(): void {
-    this.productSub.unsubscribe();
-  }
+    : 'Product Detail'; */
 
   addToCart(product: Product) {}
 }
--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -1,49 +1,32 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-
-import { NgIf, NgFor, NgClass } from '@angular/common';
-import { Product } from '../product';
+import { Component, inject } from '@angular/core';
+import { NgIf, NgFor, NgClass, AsyncPipe } from '@angular/common';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../product.service';
-import { catchError, EMPTY, Subscription, tap } from 'rxjs';
+import { catchError, EMPTY, Subscription } from 'rxjs';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent],
+  imports: [NgIf, NgFor, NgClass, ProductDetailComponent, AsyncPipe],
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent {
   private productService = inject(ProductService);
   public sub!: Subscription;
   public pageTitle = 'Products';
   public errorMessage = '';
 
   // Products
-  public products: Product[] = [];
+  public readonly products$ = this.productService.products$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
 
-  // Selected product id to highlight the entry
-  public selectedProductId: number = 0;
-
-  ngOnInit(): void {
-    this.sub = this.productService
-      .getProducts()
-      .pipe(
-        tap(() => console.log('In component pipeline')),
-        catchError(err => {
-          this.errorMessage = err;
-          return EMPTY;
-        })
-      )
-      .subscribe(products => {
-        this.products = products;
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  public readonly selectedProductId$ = this.productService.productSelected$;
 
   onSelected(productId: number): void {
-    this.selectedProductId = productId;
+    this.productService.productSelected(productId);
   }
 }
